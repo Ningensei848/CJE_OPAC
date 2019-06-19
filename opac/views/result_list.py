65ceb73da2ca251681296d2b9ce32b45bde1d92e
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2019-06-19 14:50:35 +0900

    ADD: result_list.htmlを分離して新たにいくつかのタブごとのhtmlテンプレートを追加した（これで、タブごとの検索結果が見られるようになる）

--- opac/views/result_list.py
+++ opac/views/result_list.py
@@ -11,19 +11,6 @@ class ResultListView(ListView):
     paginate_by = 20  # if pagination is desired
     biblio = BIBLIO.objects.all()
 
-    def get_queryset(self):
-        """
-        #mainpage にわたすためのクエリセット
-        """
-        queries = self.request.GET['search']
-        biblio = self.biblio
-        query = Q()
-
-        for word in queries.split():
-            query |= Q(title__icontains=word) | Q(sub_title__icontains=word) | Q(titleheading__icontains=word)
-
-        return biblio.filter(query).distinct().order_by('publishedYear').reverse()
-
     def get_context_data(self, **kwargs):
         """
         #mainpage 以外に渡すためのクエリセット
@@ -32,17 +19,79 @@ class ResultListView(ListView):
         queries = self.request.GET['search']
         context['query'] = queries
         biblio = self.biblio
-        query = Q()
 
+        # Title
+        query_title = Q()
         for word in queries.split():
-            query |= Q(responsibility__icontains=word) | Q(authorheading__icontains=word)
+            query_title |= Q(title__icontains=word) | Q(sub_title__icontains=word) | Q(titleheading__icontains=word)
+
+        page_obj_title = paginate_queryset(
+            self.request,
+            biblio.filter(query_title).distinct().order_by('publishedYear').reverse(),
+            self.paginate_by
+        )
+        context['result_title'] = page_obj_title.object_list
+        context['page_obj_title'] = page_obj_title
+
+        # responsibility
+        query_responsibility = Q()
+        for word in queries.split():
+            query_responsibility |= Q(responsibility__icontains=word) | Q(authorheading__icontains=word)
 
         page_obj_responsibility = paginate_queryset(
             self.request,
-            biblio.filter(query).distinct(),
+            biblio.filter(query_responsibility).distinct().order_by('publishedYear').reverse(),
             self.paginate_by
         )
-        context['responsibility'] = page_obj_responsibility.object_list
+        context['result_responsibility'] = page_obj_responsibility.object_list
         context['page_obj_responsibility'] = page_obj_responsibility
+
+        # publication
+        query_publication = Q()
+        for word in queries.split():
+            query_publication |= Q(publishedArea__icontains=word) | Q(publisher__icontains=word)
+
+        page_obj_publication = paginate_queryset(
+            self.request,
+            biblio.filter(query_publication).distinct().order_by('publishedYear').reverse(),
+            self.paginate_by
+        )
+        context['result_publication'] = page_obj_publication.object_list
+        context['page_obj_publication'] = page_obj_publication
+
+        # series
+        query_series = Q()
+        for word in queries.split():
+            query_series |= Q(series__icontains=word)
+
+        page_obj_series = paginate_queryset(
+            self.request,
+            biblio.filter(query_series).distinct().order_by('publishedYear').reverse(),
+            self.paginate_by
+        )
+        context['result_series'] = page_obj_series.object_list
+        context['page_obj_series'] = page_obj_series
+
+        # ANY search
+        query_any = Q()
+        for word in queries.split():
+            query_any |= Q(nbc__icontains=word)            | Q(isbn__icontains=word)           | \
+                         Q(title__icontains=word)          | Q(sub_title__icontains=word)      | \
+                         Q(responsibility__icontains=word) | Q(ed__icontains=word)             | \
+                         Q(publishedArea__icontains=word)  | Q(publisher__icontains=word)      | \
+                         Q(publishedYear__icontains=word)  | Q(publishedMonth__icontains=word) | \
+                         Q(page__icontains=word)           | Q(size__icontains=word)           | \
+                         Q(series__icontains=word)         | Q(note__icontains=word)           | \
+                         Q(titleheading__icontains=word)   | Q(authorheading__icontains=word)  | \
+                         Q(holdingsrecord__icontains=word) | Q(holdingphys__icontains=word)    | \
+                         Q(holdingloc__icontains=word)
+
+        page_obj_any = paginate_queryset(
+            self.request,
+            biblio.filter(query_any).distinct().order_by('publishedYear').reverse(),
+            self.paginate_by
+        )
+        context['result_any'] = page_obj_any.object_list
+        context['page_obj_any'] = page_obj_any
 
         return context
