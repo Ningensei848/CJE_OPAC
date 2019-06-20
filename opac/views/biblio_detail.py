fb4c618c41e2e92cb3ae53b7a05a6468b0fb807c
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2019-06-20 23:59:33 +0900

    MODIFY: 書影を追加

--- opac/views/biblio_detail.py
+++ opac/views/biblio_detail.py
@@ -1,6 +1,12 @@
 from django.views.generic import DetailView
 
 from opac.models.bibliography import BIBLIO
+from opac.mylib.isbn_tools import isbn10_to_13
+from opac.mylib.isbn_tools import confirm_source_exist
+
+import re
+
+pattern = re.compile(r"\[\'(.*?)\'")
 
 
 class BiblioDetailView(DetailView):
@@ -8,3 +14,30 @@ class BiblioDetailView(DetailView):
     template_name = 'opac/biblio_detail.html'
     slug_field = 'nbc'
     slug_url_kwarg = 'nbc'
+
+    def get_context_data(self, **kwargs):
+        """
+        書影API(openBD)から画像を持ってくる
+        """
+
+        context = super().get_context_data(**kwargs)
+        nbc = self.kwargs.get('nbc')
+        biblio = self.model
+        source = biblio.objects.get(nbc=nbc)
+        isbn = source.isbn
+        if isbn is None:
+            return context
+
+        if pattern.match(isbn):
+            m_list = pattern.findall(isbn)
+            isbn_13 = isbn10_to_13(m_list[0])
+        else:
+            # print('not match')
+            isbn_13 = isbn10_to_13(isbn)
+
+        # if img source exist...
+        img_src = confirm_source_exist(isbn_13)
+        if img_src:
+            context['openbd_img'] = img_src
+
+        return context
